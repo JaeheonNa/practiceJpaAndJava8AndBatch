80dd164fa78ee7d6dab5ee03f837e4e26ed09d2c
jaeheonNa <najh0528@gmail.com>
2023-04-21 11:11:04 +0900

    과제 완료

--- src/main/java/com/study/insuranceandbatch/InsuranceAndBatchApplication.java
+++ src/main/java/com/study/insuranceandbatch/InsuranceAndBatchApplication.java
@@ -1,15 +1,14 @@
 package com.study.insuranceandbatch;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.study.insuranceandbatch"})
-@RequiredArgsConstructor
 @EnableBatchProcessing
 public class InsuranceAndBatchApplication {
     public static void main(String[] args) {
         SpringApplication.run(InsuranceAndBatchApplication.class, args);
     }
+
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+-- insert into product (name, min_period, max_period) values ("여행자 보험", 1, 3);
+-- insert into product (name, min_period, max_period) values ("휴대폰 보험", 1, 12);
+-- insert into coverage (name, coverage, base) values ("상해 치료비", 1000000, 100);
+-- insert into coverage (name, coverage, base) values ("항공기 지연 도착 시 보상금", 500000, 100);
+-- insert into coverage (name, coverage, base) values ("부분 손실", 750000, 100);
+-- insert into coverage (name, coverage, base) values ("전체 손실", 1570000, 40);
+select 1
