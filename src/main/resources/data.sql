f04c1eb76640e2bdd9b2ea177aae2d2644428ace
jaeheonNa <najh0528@gmail.com>
2023-04-21 11:39:50 +0900

    초기 데이터 자동 삽입 기능 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,11 @@
--- insert into product (name, min_period, max_period) values ("여행자 보험", 1, 3);
--- insert into product (name, min_period, max_period) values ("휴대폰 보험", 1, 12);
--- insert into coverage (name, coverage, base) values ("상해 치료비", 1000000, 100);
--- insert into coverage (name, coverage, base) values ("항공기 지연 도착 시 보상금", 500000, 100);
--- insert into coverage (name, coverage, base) values ("부분 손실", 750000, 100);
--- insert into coverage (name, coverage, base) values ("전체 손실", 1570000, 40);
+insert into product (name, min_period, max_period, create_dtime, update_dtime) values ('여행자 보험', 1, 3, now(), now())
+insert into product (name, min_period, max_period, create_dtime, update_dtime) values ('휴대폰 보험', 1, 12, now(), now())
+insert into coverage (name, coverage, base, create_dtime, update_dtime) values ('상해 치료비', 1000000, 100, now(), now())
+insert into coverage (name, coverage, base, create_dtime, update_dtime) values ('항공기 지연 도착 시 보상금', 500000, 100, now(), now())
+insert into coverage (name, coverage, base, create_dtime, update_dtime) values ('부분 손실', 750000, 100, now(), now())
+insert into coverage (name, coverage, base, create_dtime, update_dtime) values ('전체 손실', 1570000, 40, now(), now())
+insert into product_coverage_map (product_seq, coverage_seq, use_yn, create_dtime, update_dtime) values (1,1,1,now(), now())
+insert into product_coverage_map (product_seq, coverage_seq, use_yn, create_dtime, update_dtime) values (1,2,1,now(), now())
+insert into product_coverage_map (product_seq, coverage_seq, use_yn, create_dtime, update_dtime) values (2,3,1,now(), now())
+insert into product_coverage_map (product_seq, coverage_seq, use_yn, create_dtime, update_dtime) values (2,4,1,now(), now())
 select 1
